64e512952b8fbaccf14835b01490046ee341b95c
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2024-02-17 15:22:31 +0100

    Update inventory script

--- src/script.js
+++ src/script.js
@@ -106,5 +106,10 @@ function fightSlime() {}
 function fightBeast() {}
 function sellWeapon() {
   if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
   }
 }
