51dcf42ad41172f95dfb771296f7114ea522c98d
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2024-02-19 12:44:16 +0100

    Update lose function + create restart function

--- src/script.js
+++ src/script.js
@@ -65,7 +65,7 @@ const locations = [
       "Go to town square",
     ],
     "button functions": [goTown, goTown, goTown],
-    text: "The monster screams Arg! as it dies. You gain experience points and find gold.",
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
   },
 ];
 //initialize buttons
@@ -74,6 +74,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -180,4 +181,18 @@ function defeatMonster() {
   xpText.innerText = xp;
   update(locations[4]);
 }
-function lose() {}
+function lose() {
+  update(locations[5]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
